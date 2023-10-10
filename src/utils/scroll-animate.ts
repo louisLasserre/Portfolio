115c2298b05f15d35c7fbf57d2476931354b0992
louis lasserre <louis.lasserre@mmibordeaux.com>
2023-10-11 00:26:32 +0200

    smooth operator

--- src/utils/scroll-animate.ts
+++ src/utils/scroll-animate.ts
@@ -1,112 +1,138 @@
-
 interface IAnimation {
-  animationName: string
+  animationName: string;
   elementToAnimate: HTMLElement;
-  start: number
-  end: number
-
-
+  start: number;
+  end: number;
 }
 
-let animations: IAnimation[] = []
+let animations: IAnimation[] = [];
 
-// window.addEventListener('DOMContentLoaded', (event) => {
-//   setAnimations();
+window.addEventListener("DOMContentLoaded", (event) => {
+  //setAnimations();
+});
 
+function animateScrollDown(element: HTMLElement, percent: number) {
+  element.style.transform = `translate(-50%, -50%) rotate(${percent * 1.2}deg)`;
+}
 
-// });
-// document.addEventListener('scroll', (event) => {
+const animateElement = document.querySelector("[data-animate]");
+document.addEventListener("scroll", (event) => {
+  const { top, bottom } = animateElement?.getBoundingClientRect();
 
-//   let actualScreenTopScroll = window.scrollY
-//   let actualScreenBottomScroll = window.scrollY + window.screen.availHeight
+  //console.log(top - window.innerHeight, bottom);
 
-//   console.log(actualScreenTopScroll, actualScreenBottomScroll);
+  if (top - window.innerHeight < 0 && bottom > 0) {
+    const topAtBot = top - window.innerHeight;
+    const totalDistance = window.innerHeight + (bottom - top);
+    const positiveTopDistance = Math.abs(topAtBot);
 
+    const percentOfAnimation =
+      ((totalDistance + positiveTopDistance) * 100) / totalDistance - 100;
+    animateScrollDown(animateElement, percentOfAnimation);
+  }
 
-//   let animate = animations.filter((animation) => animation.end > actualScreenTopScroll && animation.start < actualScreenBottomScroll)
-//   console.log({animate});
+  // let actualScreenTopScroll = window.scrollY;
+  // let actualScreenBottomScroll = window.scrollY + window.screen.availHeight;
 
-//   animate.forEach(animation => {
-//     let test = animation.animationName
-//     test()
-//   });
+  // console.log(actualScreenTopScroll, actualScreenBottomScroll);
 
+  // let animate = animations.filter(
+  //   (animation) =>
+  //     animation.end > actualScreenTopScroll &&
+  //     animation.start < actualScreenBottomScroll
+  // );
+  // console.log({ animate });
 
-// });
+  // animate.forEach((animation) => {
+  //   let test = animation.animationName;
+  //   console.log(test);
+
+  //   //test();
+  // });
+});
 
 const test = (percent: number, offset: number) => {
-  console.table({percent, offset});
-
-}
+  console.log({ percent, offset });
+};
 
 const setAnimations: () => void = () => {
-  let elementsToAnimate: HTMLElement[] = Array.from(document.querySelectorAll('[data-animate]'))
+  let elementsToAnimate: HTMLElement[] = Array.from(
+    document.querySelectorAll("[data-animate]")
+  );
   console.log(elementsToAnimate);
 
-  elementsToAnimate.forEach(element => {
-    const animation = getAnimation(element)
+  elementsToAnimate.forEach((element) => {
+    const animation = getAnimation(element);
     if (animation) {
-      animations.push(animation)
+      animations.push(animation);
     }
   });
   console.log(animations);
+};
 
-}
-
-const getAnimation: (element: HTMLElement) => IAnimation | undefined = (element) => {
-  let animationName: string = element.attributes['data-animate'].value
+const getAnimation: (element: HTMLElement) => IAnimation | undefined = (
+  element
+) => {
+  let animationName: string = element.attributes["data-animate"].value;
   if (!animationName) {
-    showError("every animation must have a name, your html attribute 'data-animate' must you like: data-animate='nameOfYourAnimation' ")
-    return
+    showError(
+      "every animation must have a name, your html attribute 'data-animate' must be like: data-animate='nameOfYourAnimation' "
+    );
+    return;
   }
-  let start = getStart(animationName)
-  let end = getEnd(animationName)
+  let start = getStart(animationName);
+  let end = getEnd(animationName);
 
   if (!start || !end) {
-    return
+    return;
   }
 
   return {
     animationName,
     elementToAnimate: element,
     start,
-    end
-  }
+    end,
+  };
+};
 
-}
-
-const getStart = (animationName: IAnimation['animationName']) => {
-  let startElement: HTMLElement | null = document.querySelector(`[data-animate-${animationName}-start]`)
+const getStart = (animationName: IAnimation["animationName"]) => {
+  let startElement: HTMLElement | null = document.querySelector(
+    `[data-animate-${animationName}-start]`
+  );
 
   if (!startElement) {
-    showError("you must specify a starting and ending position for each animation by using attributes data-animate-animationName-start and data-animate-animationName-end")
-    return
+    showError(
+      "you must specify a starting and ending position for each animation by using attributes data-animate-animationName-start and data-animate-animationName-end"
+    );
+    return;
   }
 
-  startElement.style.background = 'red'
+  startElement.style.background = "red";
 
-  let startElementPos = startElement.getBoundingClientRect()
-  return startElementPos.y
+  let startElementPos = startElement.getBoundingClientRect();
+  console.log(startElementPos);
 
-}
-const getEnd = (animationName: IAnimation['animationName']) => {
-  let endElement: HTMLElement | null = document.querySelector(`[data-animate-${animationName}-end]`)
-
+  return startElementPos.y;
+};
+const getEnd = (animationName: IAnimation["animationName"]) => {
+  let endElement: HTMLElement | null = document.querySelector(
+    `[data-animate-${animationName}-end]`
+  );
 
   if (!endElement) {
-    showError("you must specify a starting and ending position for each animation by using attributes data-animate-animationName-start and data-animate-animationName-end")
-    return
+    showError(
+      "you must specify a starting and ending position for each animation by using attributes data-animate-animationName-start and data-animate-animationName-end"
+    );
+    return;
   }
 
-  endElement.style.background = 'green'
+  endElement.style.background = "green";
 
-  let endElementPos = endElement.getBoundingClientRect()
+  let endElementPos = endElement.getBoundingClientRect();
 
-  return endElementPos.y
+  return endElementPos.y;
+};
 
-}
-
-
-const showError = (err: string) =>  {
-  console.error(err)
-}
+const showError = (err: string) => {
+  console.error(err);
+};
