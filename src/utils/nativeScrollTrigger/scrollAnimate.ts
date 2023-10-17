6760f31eb32b3b20734ba37b4c123a9a48a95cc9
louis lasserre <louis.lasserre@mmibordeaux.com>
2023-10-17 19:32:38 +0200

    fix accessibility

--- src/utils/nativeScrollTrigger/scrollAnimate.ts
+++ src/utils/nativeScrollTrigger/scrollAnimate.ts
@@ -1,26 +1,28 @@
 export function animateElementonScroll(
-  element: any,
-  callback: (element: any, percentOfAnimation: number) => void,
+  element: HTMLElement | Element,
+  callback: (
+    element: HTMLElement | Element,
+    percentOfAnimation: number
+  ) => void,
   options?: {
     endInset: number;
     startInset: number;
   }
 ) {
-  const { top, bottom } = element?.getBoundingClientRect();
+  const { top, bottom } = element.getBoundingClientRect();
 
   const { endInset, startInset } = options ?? {};
 
-  const end = endInset ? bottom - window.innerHeight * endInset : bottom;
-  const start = startInset
+  const realTimeEnd = endInset
+    ? bottom - window.innerHeight * endInset
+    : bottom;
+  const realTimeStart = startInset
     ? top - window.innerHeight + startInset * window.innerHeight
     : top - window.innerHeight;
 
-  //console.log(bottom, end, window.innerHeight * endInset);
-  //const getPercenta = (!endInset && !startInset) ? 1 : ()
-
   const offset = 1 - ((endInset || 0) + (startInset || 0));
 
-  if (start < 0 && end > 0) {
+  if (realTimeStart < 0 && realTimeEnd > 0) {
     const elementH = bottom - top;
     const totalDistance = window.innerHeight * offset + elementH;
 
@@ -30,6 +32,7 @@ export function animateElementonScroll(
     const startingPoint = startInset
       ? window.innerHeight - window.innerHeight * startInset
       : window.innerHeight;
+    console.log(startingPoint, realTimeStart);
 
     const percentOfAnimation = ((startingPoint - top) * 100) / totalDistance;
 
